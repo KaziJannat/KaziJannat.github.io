7958e4a35890d7a554c893214ff6d13a3ce125f2
KaziJannat <156371873+KaziJannat@users.noreply.github.com>
2024-03-18 11:31:05 -0400

    add images

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -7,13 +7,18 @@ const overlay = document.querySelector('.overlay');
 
 
 /* Declaring the array of image filenames */
-
+const filenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
 /* Declaring the alternative text for each image file */
-
+const alternativeText = ['Closeup of a human eye', 'Closeup of a natural rock', 'Closeup of a flower', 'Closeup of a flower', 'Closeup of a batarflay']
 
 /* Looping through images */
-
+filenames.forEach((src, index) => {
+    const img = document.createElement('img');
+    img.src = `images/${src}`;
+    img.alt = alternativeText[index];
+    thumbBar.appendChild(img)
+});
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
